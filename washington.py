fd3fc5f874b0ca0cc1a0aa815f07c12ba7ef4805
TalentBoss <workmail1candy@gmail.com>
2023-10-21 05:55:11 -0700

    b
    b

--- washington.py
+++ washington.py
@@ -48,6 +48,16 @@ def is_email_format(string):
     else:
         return False
 
+def get_substring(string):
+    start_index = string.find('of') + 3
+    end_index = string.rfind(',')
+
+    if start_index >= 0 and end_index >= 0 and end_index > start_index:
+        substring = string[start_index:end_index]
+        return substring
+    else:
+        return None
+
 
 # configure webdriver
 options = Options()
@@ -105,113 +115,107 @@ pagination_lis = pagination_ul.find_elements(By.TAG_NAME, 'li')
 
 table_element = driver.find_element(By.CSS_SELECTOR, 'table.table')
 table_trs = table_element.find_elements(By.CSS_SELECTOR, 'tbody tr')
+page_num_str = get_substring(page_span.text)
+if not page_num_str == None:
+    page_num = int(page_num_str)
+    print(page_num)
+# for i in range(4):
+#     time.sleep(3)
+#     pagination_lis[len(pagination_lis) - 2].click()
 
+# for k in range(0, len(pagination_lis)):
 # for j in range(0, len(table_trs)-1):
-table_element = driver.find_element(By.CSS_SELECTOR, 'table.table')
-table_trs = table_element.find_elements(By.CSS_SELECTOR, 'tbody tr')
-# print(len(table_trs))
-time.sleep(1)
-table_tds = table_trs[0].find_element(By.CSS_SELECTOR, 'td a')
-try:
-    table_tds.click()
-    time.sleep(2)
-    history_btn = driver.find_element(By.ID, 'btnFilingHistory')
-    history_btn.click()
-    time.sleep(2)
-    pdf_table = driver.find_elements(By.CSS_SELECTOR, 'table.table')
-    pdf_table_tbody = pdf_table[0].find_element(By.CSS_SELECTOR, 'tbody')
-    pdf_table_trs = pdf_table_tbody.find_elements(By.CSS_SELECTOR, 'tr')
-    pdf_table_tds = pdf_table_trs[len(pdf_table_trs) - 1].find_elements(By.CSS_SELECTOR, 'td')
-    view_pdf_a = pdf_table_tds[len(pdf_table_tds) - 1].find_element(By.TAG_NAME, 'a')
-    time.sleep(1)
-    view_pdf_a.click()
-    time.sleep(4)
-    
-    final_pdf_table_tbodies = pdf_table[1].find_elements(By.CSS_SELECTOR, 'tbody')
-    final_pdf_table_trs = final_pdf_table_tbodies[len(final_pdf_table_tbodies) - 2].find_elements(By.CSS_SELECTOR, 'tr')
-    final_pdf_table_tds = final_pdf_table_trs[len(final_pdf_table_trs) - 1].find_elements(By.CSS_SELECTOR, 'td')
-    final_pdf_table_i = final_pdf_table_tds[len(final_pdf_table_tds) - 1].find_element(By.TAG_NAME, 'i')
-    time.sleep(2)
-    final_pdf_table_i.click()
-    time.sleep(4)
-    
-    
-    list_of_files = glob.glob('C:/Users/Christian/Downloads/*') # * means all if need specific format then *.csv
-    latest_file = max(list_of_files, key=os.path.getctime)
-    # print(len(list_of_files))
+#     table_element = driver.find_element(By.CSS_SELECTOR, 'table.table')
+#     table_trs = table_element.find_elements(By.CSS_SELECTOR, 'tbody tr')
+#     time.sleep(1)
+#     table_tds = table_trs[j].find_element(By.CSS_SELECTOR, 'td a')
+#     try:
+#         table_tds.click()
+#         time.sleep(2)
+#         history_btn = driver.find_element(By.ID, 'btnFilingHistory')
+#         history_btn.click()
+#         time.sleep(2)
+#         pdf_table = driver.find_elements(By.CSS_SELECTOR, 'table.table')
+#         pdf_table_tbody = pdf_table[0].find_element(By.CSS_SELECTOR, 'tbody')
+#         pdf_table_trs = pdf_table_tbody.find_elements(By.CSS_SELECTOR, 'tr')
+#         pdf_table_tds = pdf_table_trs[len(pdf_table_trs) - 1].find_elements(By.CSS_SELECTOR, 'td')
+#         view_pdf_a = pdf_table_tds[len(pdf_table_tds) - 1].find_element(By.TAG_NAME, 'a')
+#         time.sleep(1)
+#         view_pdf_a.click()
+#         time.sleep(4)
+        
+#         final_pdf_table_tbodies = pdf_table[1].find_elements(By.CSS_SELECTOR, 'tbody')
+#         final_pdf_table_trs = final_pdf_table_tbodies[len(final_pdf_table_tbodies) - 2].find_elements(By.CSS_SELECTOR, 'tr')
+#         final_pdf_table_tds = final_pdf_table_trs[len(final_pdf_table_trs) - 1].find_elements(By.CSS_SELECTOR, 'td')
+#         final_pdf_table_i = final_pdf_table_tds[len(final_pdf_table_tds) - 1].find_element(By.TAG_NAME, 'i')
+#         time.sleep(2)
+#         final_pdf_table_i.click()
+#         time.sleep(4)
+        
+        
+#         list_of_files = glob.glob('C:/Users/Christian/Downloads/*') # * means all if need specific format then *.csv
+#         latest_file = max(list_of_files, key=os.path.getctime)
+#         # print(len(list_of_files))
 
-    
-    
-    # creating a pdf reader object 
-    reader = PdfReader(latest_file) 
-    
-    # printing number of pages in pdf file 
-    
-    
-    # getting a specific page from the pdf file 
-    text = ''
-    for i in range(0, len(reader.pages)):
-        page = reader.pages[i]
-        page_text = page.extract_text()
-        # extracting text from page 
-        text = text + page_text
+        
+        
+#         # creating a pdf reader object 
+#         reader = PdfReader(latest_file) 
+        
+#         # printing number of pages in pdf file 
+        
+        
+#         # getting a specific page from the pdf file 
+#         text = ''
+#         for i in range(0, len(reader.pages)):
+#             page = reader.pages[i]
+#             page_text = page.extract_text()
+#             # extracting text from page 
+#             text = text + page_text
 
-    try:
-        lines = text.splitlines()
-        ubi_number = ''
-        business_name = ''
-        phone_number = ''
-        email = ''
-        address = ''
-        first_name = ''
-        last_name = ''
-        for i in range(len(lines)):
-            if 'UBI Number:' in lines[i]:  # Case-sensitive match
-                if i + 1 < len(lines):
-                    if is_contains_number(lines[i + 1]):
-                        ubi_number = lines[i + 1]
-            if 'Business Name' in lines[i]: 
-                if i + 1 < len(lines):
-                    business_name = lines[i + 1]
-            if 'Phone:' in lines[i]:
-                if i + 1 < len(lines):
-                    if is_contains_number(lines[i + 1]):
-                        phone_number = lines[i + 1]
-            if 'Email:' in lines[i] and email == '':
-                if i + 1 < len(lines):
-                    if is_email_format(lines[i + 1]):
-                        email = lines[i + 1]
-            if 'Street Address:' in lines[i]:
-                if i + 1 < len(lines):
-                    address = lines[i + 1]
-            if 'First Name:' in lines[i]:
-                if i + 1 < len(lines):
-                    first_name = lines[i + 1]
-            if 'Last Name:' in lines[i]:
-                if i + 1 < len(lines):
-                    last_name = lines[i + 1]
-        print(ubi_number + '  =======  ' + business_name + '  ==========  ' + phone_number + '  ==========  ' + email + '  =======  ' + address + '  ==========  ' + first_name + '  ==========  ' + last_name)
-    except Exception as e:
-        print(f"{e}")
-    driver.back()
-    time.sleep(3)
-    driver.back()
-    time.sleep(3)
-except Exception as e:
-    print(f"{e}")
-    # table_a = table_tds[0].find_element(By.CSS_SELECTOR, 'a')
-    # table_a.click()
-
-    # driver.execute_script("arguments[0].ng-click();", table_a)
-    # time.sleep(5)
-    # history_btn = driver.find_element(By.ID, 'btnFilingHistory')
-    # history_btn.click()
-
-    # pdf_table = driver.find_element(By.CLASS_NAME, 'table-striped')
-    # pdf_table_tbody = driver.find_elements(By.CSS_SELECTOR, 'table.table-striped tbody')[0]
-    # pdf_table_trs = pdf_table_tbody.find_elements(By.CSS_SELECTOR, 'tr')
-    # print(len(pdf_table_trs))
-    # driver.back()
+#         try:
+#             lines = text.splitlines()
+#             ubi_number = ''
+#             business_name = ''
+#             phone_number = ''
+#             email = ''
+#             address = ''
+#             first_name = ''
+#             last_name = ''
+#             for i in range(len(lines)):
+#                 if 'UBI Number:' in lines[i]:  # Case-sensitive match
+#                     if i + 1 < len(lines):
+#                         if is_contains_number(lines[i + 1]):
+#                             ubi_number = lines[i + 1]
+#                 if 'Business Name' in lines[i]: 
+#                     if i + 1 < len(lines):
+#                         business_name = lines[i + 1]
+#                 if 'Phone:' in lines[i]:
+#                     if i + 1 < len(lines):
+#                         if is_contains_number(lines[i + 1]):
+#                             phone_number = lines[i + 1]
+#                 if 'Email:' in lines[i] and email == '':
+#                     if i + 1 < len(lines):
+#                         if is_email_format(lines[i + 1]):
+#                             email = lines[i + 1]
+#                 if 'Street Address:' in lines[i]:
+#                     if i + 1 < len(lines):
+#                         address = lines[i + 1]
+#                 if 'First Name:' in lines[i]:
+#                     if i + 1 < len(lines):
+#                         first_name = lines[i + 1]
+#                 if 'Last Name:' in lines[i]:
+#                     if i + 1 < len(lines):
+#                         last_name = lines[i + 1]
+#             print(ubi_number + '  =======  ' + business_name + '  ==========  ' + phone_number + '  ==========  ' + email + '  =======  ' + address + '  ==========  ' + first_name + '  ==========  ' + last_name)
+#         except Exception as e:
+#             print(f"{e}")
+#         driver.back()
+#         time.sleep(3)
+#         driver.back()
+#         time.sleep(3)
+#     except Exception as e:
+#         print(f"{e}")
 
 # for i in range(4):
 #     time.sleep(3)
